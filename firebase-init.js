dda6adca98773eac3d8b0fe3b04831c3b65f86c0
MAHFOOR CNC <mahfour71@gmail.com>
2025-11-20 01:01:14 +0200

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,20 +1,18 @@
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
+const firebaseConfig = {
+  apiKey: "AIzaSyCOoh1GVhGhA4g7M9ptprPRnTrszpSexmU",
+  authDomain: "mahfoor-cnc-6b389.firebaseapp.com",
+  databaseURL: "https://mahfoor-cnc-6b389-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "mahfoor-cnc-6b389",
+  storageBucket: "mahfoor-cnc-6b389.appspot.com",
+  messagingSenderId: "422714394058",
+  appId: "1:422714394058:web:d3dab2bf9f64b27cc5fb33"
+};
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyCOoh1GVhGhA4g7M9ptprPRnTrszpSexmU",
-    authDomain: "mahfoor-cnc-6b389.firebaseapp.com",
-    databaseURL: "https://mahfoor-cnc-6b389-default-rtdb.firebaseio.com",
-    projectId: "mahfoor-cnc-6b389",
-    storageBucket: "mahfoor-cnc-6b389.firebasestorage.app",
-    messagingSenderId: "422714394058",
-    appId: "1:422714394058:web:d3dab2bf9f64b27cc5fb33"
-  };
-
+// Initialize Firebase
+if (typeof firebase === 'undefined') {
+  console.error('Firebase SDK not loaded. Make sure firebase-app and firebase-database scripts are included before firebase-init.js');
+} else {
   firebase.initializeApp(firebaseConfig);
-  window.db = firebase.database();
-
-  console.log("%cFirebase متصل بنجاح يا وحش! ✅🔥", "color: #25D366; font-size: 18px; font-weight: bold;");
-</script>
-
+  // expose a consistent global database reference
+  window.db = window.db || firebase.database();
+}
